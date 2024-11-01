527677ee92215809a46a760d49168158a68416fe
47-z <160566250+47-z@users.noreply.github.com>
2024-11-01 01:12:11 -0400

    Add new colors+
    Add lilac, sapphire, forest, and periwinkle. Change powder blue to sky. Change rose to peach.

--- MoorColors/main.gd
+++ MoorColors/main.gd
@@ -4,12 +4,20 @@ const ID = "MoorColors"
 onready var Lure = get_node("/root/SulayreLure")
 
 func _ready():
-	#primary
-	Lure.add_content(ID,"pcolor_powderblue","mod://Primary/pcolor_powderblue.tres")
+	# Primary
+	Lure.add_content(ID,"pcolor_sky","mod://Primary/pcolor_sky.tres")
 	Lure.add_content(ID,"pcolor_lavender","mod://Primary/pcolor_lavender.tres")
-	Lure.add_content(ID,"pcolor_rose","mod://Primary/pcolor_rose.tres")
+	Lure.add_content(ID,"pcolor_peach","mod://Primary/pcolor_peach.tres")
+	Lure.add_content(ID,"pcolor_lilac","mod://Primary/pcolor_lilac.tres")
+	Lure.add_content(ID,"pcolor_sapphire","mod://Primary/pcolor_sapphire.tres")
+	Lure.add_content(ID,"pcolor_forest","mod://Primary/pcolor_forest.tres")
+	Lure.add_content(ID,"pcolor_periwinkle","mod://Primary/pcolor_periwinkle.tres")
 	
-	#secondary
-	Lure.add_content(ID,"scolor_powderblue","mod://Secondary/scolor_powderblue.tres")
+	# Secondary
+	Lure.add_content(ID,"scolor_sky","mod://Secondary/scolor_sky.tres")
 	Lure.add_content(ID,"scolor_lavender","mod://Secondary/scolor_lavender.tres")
-	Lure.add_content(ID,"scolor_rose","mod://Secondary/scolor_rose.tres")
+	Lure.add_content(ID,"scolor_peach","mod://Secondary/scolor_peach.tres")
+	Lure.add_content(ID,"scolor_lilac","mod://Secondary/scolor_lilac.tres")
+	Lure.add_content(ID,"scolor_sapphire","mod://Secondary/scolor_sapphire.tres")
+	Lure.add_content(ID,"scolor_forest","mod://Secondary/scolor_forest.tres")
+	Lure.add_content(ID,"scolor_periwinkle","mod://Secondary/scolor_periwinkle.tres")
