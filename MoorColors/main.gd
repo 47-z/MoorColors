1716358732534ff301aa059023cec710d0b2cb86
47-z <160566250+47-z@users.noreply.github.com>
2024-11-07 18:27:57 -0500

    Add 7 new colors

--- MoorColors/main.gd
+++ MoorColors/main.gd
@@ -4,20 +4,58 @@ const ID = "MoorColors"
 onready var Lure = get_node("/root/SulayreLure")
 
 func _ready():
-	# Primary
+	# Sky
 	Lure.add_content(ID,"pcolor_sky","mod://Primary/pcolor_sky.tres")
-	Lure.add_content(ID,"pcolor_lavender","mod://Primary/pcolor_lavender.tres")
-	Lure.add_content(ID,"pcolor_peach","mod://Primary/pcolor_peach.tres")
-	Lure.add_content(ID,"pcolor_lilac","mod://Primary/pcolor_lilac.tres")
-	Lure.add_content(ID,"pcolor_sapphire","mod://Primary/pcolor_sapphire.tres")
-	Lure.add_content(ID,"pcolor_forest","mod://Primary/pcolor_forest.tres")
-	Lure.add_content(ID,"pcolor_periwinkle","mod://Primary/pcolor_periwinkle.tres")
-	
-	# Secondary
 	Lure.add_content(ID,"scolor_sky","mod://Secondary/scolor_sky.tres")
+	
+	# Lavender
+	Lure.add_content(ID,"pcolor_lavender","mod://Primary/pcolor_lavender.tres")
 	Lure.add_content(ID,"scolor_lavender","mod://Secondary/scolor_lavender.tres")
+	
+	# Peach
+	Lure.add_content(ID,"pcolor_peach","mod://Primary/pcolor_peach.tres")
 	Lure.add_content(ID,"scolor_peach","mod://Secondary/scolor_peach.tres")
+	
+	# Lilac
+	Lure.add_content(ID,"pcolor_lilac","mod://Primary/pcolor_lilac.tres")
 	Lure.add_content(ID,"scolor_lilac","mod://Secondary/scolor_lilac.tres")
+	
+	# Sapphire
+	Lure.add_content(ID,"pcolor_sapphire","mod://Primary/pcolor_sapphire.tres")
 	Lure.add_content(ID,"scolor_sapphire","mod://Secondary/scolor_sapphire.tres")
+	
+	# Forest
+	Lure.add_content(ID,"pcolor_forest","mod://Primary/pcolor_forest.tres")
 	Lure.add_content(ID,"scolor_forest","mod://Secondary/scolor_forest.tres")
+		
+	# Periwinkle
+	Lure.add_content(ID,"pcolor_periwinkle","mod://Primary/pcolor_periwinkle.tres")
 	Lure.add_content(ID,"scolor_periwinkle","mod://Secondary/scolor_periwinkle.tres")
+
+	# Clementine
+	Lure.add_content(ID,"pcolor_clementine","mod://Primary/pcolor_clementine.tres")
+	Lure.add_content(ID,"scolor_clementine","mod://Secondary/scolor_clementine.tres")
+	
+	# Indigo
+	Lure.add_content(ID,"pcolor_indigo","mod://Primary/pcolor_indigo.tres")
+	Lure.add_content(ID,"scolor_indigo","mod://Secondary/scolor_indigo.tres")
+	
+	# Lime
+	Lure.add_content(ID,"pcolor_lime","mod://Primary/pcolor_lime.tres")
+	Lure.add_content(ID,"scolor_lime","mod://Secondary/scolor_lime.tres")
+	
+	# River
+	Lure.add_content(ID,"pcolor_river","mod://Primary/pcolor_river.tres")
+	Lure.add_content(ID,"scolor_river","mod://Secondary/scolor_river.tres")
+	
+	# Sprout
+	Lure.add_content(ID,"pcolor_sprout","mod://Primary/pcolor_sprout.tres")
+	Lure.add_content(ID,"scolor_sprout","mod://Secondary/scolor_sprout.tres")
+
+	# Lupine
+	Lure.add_content(ID,"pcolor_lupine","mod://Primary/pcolor_lupine.tres")
+	Lure.add_content(ID,"scolor_lupine","mod://Secondary/scolor_lupine.tres")
+	
+	# Buttercup
+	Lure.add_content(ID,"pcolor_buttercup","mod://Primary/pcolor_buttercup.tres")
+	Lure.add_content(ID,"scolor_buttercup","mod://Secondary/scolor_buttercup.tres")
